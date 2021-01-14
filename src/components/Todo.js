9c559dc866009594e1d39fc0174414cd7d958700
Helga312 <may.traum@gmail.com>
2021-01-14 16:46:43 +0300

    feat: add styling and cross browser prefixes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Todo = ({ text, todo, todos, setTodos }) => {
     //Events
     const deleteHandler = () => {
-        setTodos(todos.filter(el => el.id !== todo.id))
+        setTodos(todos.filter(el => el.id !== todo.id));
     };
     const completeHandler = () => {
         setTodos(todos.map((item) => {
